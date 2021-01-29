55af50bcc7f6db771bb56c8bd351ae64870b8e88
Luke Rucker <ruckerlh@gmail.com>
2021-01-29 21:54:02 +0100

    Members Routing

--- src/pages/members.js
+++ src/pages/members.js
@@ -1,7 +1,27 @@
 import React from 'react'
+import { useRouteMatch, useParams, Switch, Route, Link } from 'react-router-dom'
+
+function MemberTable() {
+    const { url } = useRouteMatch()
+}
+
+function Member() {
+    const { memberId } = useParams()
+}
 
 function Members() {
-    return <h1>Members</h1>
+    const { path } = useRouteMatch()
+
+    return (
+        <Switch>
+            <Route exact path={path}>
+                <MemberTable />
+            </Route>
+            <Route path={`${path}/:memberId`}>
+                <Member />
+            </Route>
+        </Switch>
+    )
 }
 
 export default Members
