f60c5e51ac044c95858fe275616bcb4d400646a0
Luke Rucker <ruckerlh@gmail.com>
2021-02-24 15:14:21 +0100

    404

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,3 @@
-export { default as Alert } from './alert'
 export { default as AppBar } from './app-bar'
 export { default as FullPageSpinner } from './full-page-spinner'
 export { default as MembersTable } from './members-table'
--- src/screens/four-o-four.js
+++ src/screens/four-o-four.js
@@ -1,43 +1,35 @@
 import React from 'react'
-import { makeStyles } from '@material-ui/core/styles'
-import { Typography } from '@material-ui/core'
-import SentimentVeryDissatisfiedIcon from '@material-ui/icons/SentimentVeryDissatisfied'
-import AppShell from '../app-shell'
+import { Grid, Container, Icon, Header } from 'semantic-ui-react'
 import { AppBar } from '../components'
 import { useAuth } from '../context/auth-context'
 
-const useStyles = makeStyles(theme => ({
-    // necessary for content to be below app bar
-    toolbar: theme.mixins.toolbar,
-    message: {
-        textAlign: 'center',
-        padding: theme.spacing(2),
-    },
-}))
-
 function FourOFour() {
-    const classes = useStyles()
     const { isAuthenticated } = useAuth()
-
-    const message = (
-        <div className={classes.message}>
-            <SentimentVeryDissatisfiedIcon style={{ fontSize: 80 }} />
-            <Typography variant="h3">You look lost!</Typography>
-        </div>
-    )
 
     return (
         <>
             {isAuthenticated() ? (
-                <AppShell>{message}</AppShell>
+                <FourOFourMessage />
             ) : (
                 <>
                     <AppBar />
-                    <div className={classes.toolbar} />
-                    {message}
+                    <FourOFourMessage />
                 </>
             )}
         </>
+    )
+}
+
+function FourOFourMessage() {
+    return (
+        <Grid style={{ height: '90vh' }} verticalAlign="middle">
+            <Grid.Column>
+                <Container text textAlign="center">
+                    <Icon name="frown outline" size="huge" />
+                    <Header size="huge">You look lost!</Header>
+                </Container>
+            </Grid.Column>
+        </Grid>
     )
 }
 
