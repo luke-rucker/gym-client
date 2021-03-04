2b057550c43e1898d4fdd7af692a50f15e19e7ba
Luke Rucker <ruckerlh@gmail.com>
2021-03-04 22:21:49 +0100

    Progress towards adding active sessions to dashboard

--- src/screens/dashboard.js
+++ src/screens/dashboard.js
@@ -1,7 +1,77 @@
 import React from 'react'
+import { useQuery } from 'react-query'
+import { Statistic, Grid, Button, Icon, Table } from 'semantic-ui-react'
+import { FullPageErrorFallback, FullPageSpinner } from '../components'
+import { useAxios } from '../context/axios-context'
 
 function Dashboard() {
-    return <h1>Dashboard</h1>
+    const axios = useAxios()
+
+    const { isLoading, error, data } = useQuery('activeSessions', () =>
+        axios.get('/sessions/?status=active').then(response => response.data)
+    )
+
+    if (isLoading) {
+        return <FullPageSpinner />
+    }
+
+    if (error) {
+        return <FullPageErrorFallback message={error.response.data.message} />
+    }
+
+    return (
+        <>
+            <Statistic.Group widths="1">
+                <Statistic>
+                    <Statistic.Value>{data.length}/15</Statistic.Value>
+                    <Statistic.Label>Members in the Gym</Statistic.Label>
+                </Statistic>
+            </Statistic.Group>
+            <Grid>
+                <Grid.Column width={4} floated="right">
+                    <Button
+                        floated="right"
+                        icon
+                        labelPosition="left"
+                        color="green"
+                    >
+                        <Icon name="plus" />
+                        Check in member
+                    </Button>
+                </Grid.Column>
+            </Grid>
+            <Table>
+                <Table.Header>
+                    <Table.Row>
+                        <Table.HeaderCell>Member</Table.HeaderCell>
+                        <Table.HeaderCell>Time Left</Table.HeaderCell>
+                        <Table.HeaderCell textAlign="right">
+                            Check Out
+                        </Table.HeaderCell>
+                    </Table.Row>
+                </Table.Header>
+                {data.map(session => (
+                    <Table.Body>
+                        <Table.Row>
+                            <Table.Cell>{`${session.member.firstName} ${session.member.lastName}`}</Table.Cell>
+                            <Table.Cell>
+                                <TimeLeft
+                                    sessionStart={new Date(session.start)}
+                                />
+                            </Table.Cell>
+                            <Table.Cell textAlign="right">
+                                <Button>Check Out</Button>
+                            </Table.Cell>
+                        </Table.Row>
+                    </Table.Body>
+                ))}
+            </Table>
+        </>
+    )
+}
+
+function TimeLeft({ sessionStart }) {
+    return <span>{sessionStart.toString()}</span>
 }
 
 export default Dashboard
